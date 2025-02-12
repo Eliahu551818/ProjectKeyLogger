b3e590ed2cc736e22a261184db6411cafbc555b2
Or Zellinger <or.zellinger@gmail.com>
2025-02-12 17:06:50 +0200

    Add comments

--- key_logger_service/i_key_logger_service.py
+++ key_logger_service/i_key_logger_service.py
@@ -1,15 +1,19 @@
 from abc import ABC, abstractmethod
 
+# An Interface To KeyLoggerService
 class IKeloggerService(ABC):
 
+    # Start Method starts the service
     @abstractmethod
     def start(self):
         pass
 
+    # Stop Method stops the service
     @abstractmethod
     def stop(self):
         pass
 
+    # Get Method Returns the data that stored
     @abstractmethod
     def get(self):
         pass

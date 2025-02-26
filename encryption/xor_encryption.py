e249ed577585dc6c5538111eddfbd9ee0ec8cf21
eliahu <eliahubenamotz@gmail.com>
2025-02-26 12:38:08 +0200

    type of input for encryption

--- encryption/xor_encryption.py
+++ encryption/xor_encryption.py
@@ -1,13 +1,11 @@
 from .encryption_interface import IEncryptor
 
 class XOrEncryption(IEncryptor):
-    def encrypt(self, data):
+    def encrypt(self, data: str):
         key = "interpreter"
         return ''.join(chr(ord(c) ^ ord(key[i % len(key)])) for i, c in enumerate(data))
 
 
-    def decrypt(self, data):
+    def decrypt(self, data: str):
         key = "interpreter"
         return ''.join(chr(ord(c) ^ ord(key[i % len(key)])) for i, c in enumerate(data))
-
-        
